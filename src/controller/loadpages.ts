5d47bc0c5fc86aca735caf67db581d28330a88a4
Magiciaan <surajkhadka315@gmail.com>
2023-04-04 13:25:09 +0545

    fix: small params error

--- src/controller/loadpages.ts
+++ src/controller/loadpages.ts
@@ -2,16 +2,18 @@ import { RequestHandler } from "express";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export const home: RequestHandler = async (req, res) => {
-  if (req.url == "/home") {
+export const home: RequestHandler = async (_req, res) => {
+  try {
     const allUsers = await prisma.user.findMany();
     res.status(201).json({ allUsers });
+  } catch (error) {
+    res.status(400).json(error);
   }
 };
 
 export const register: RequestHandler = async (req, res) => {
-  if (req.url == "/register") {
-    const { name, email } = req.body;
+  const { name, email } = req.body;
+  try {
     const user = await prisma.user.create({
       data: {
         name,
@@ -19,25 +21,32 @@ export const register: RequestHandler = async (req, res) => {
       },
     });
     res.status(201).json({ user });
+  } catch (error) {
+    res.status(400).json(error);
   }
 };
 
 export const edit: RequestHandler = async (req, res) => {
   const id: number = parseInt(req.params.id);
   const { email, name } = req.body;
-  const update = await prisma.user.update({
-    where: { id },
-    data: { email, name },
-  });
-  res.status(201).json(update);
+  try {
+    const update = await prisma.user.update({
+      where: { id },
+      data: { email, name },
+    });
+    res.status(201).json(update);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 export const remove: RequestHandler = async (req, res) => {
-  if (!req.params.id) {
-    throw new Error("no ID given");
+  try {
+    const del = await prisma.user.delete({
+      where: { id: parseInt(req.params.id) },
+    });
+    res.status(201).json({ deletedUser: del });
+  } catch (error) {
+    res.status(400).json(error);
   }
-  const del = await prisma.user.delete({
-    where: { id: parseInt(req.params.id) },
-  });
-  res.status(201).json({ deletedUser: del });
 };
